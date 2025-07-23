d23b4d9cc5ba3a2f7a6aa672cf06da4e9d63f610
Iraniya <iraniyanaynesh@gmail.com>
2025-07-23 19:07:34 +0530

    Chat view added with custom background image

--- CoreMLHikingSurvey/ContentView.swift
+++ CoreMLHikingSurvey/ContentView.swift
@@ -27,6 +27,7 @@ struct ContentView: View {
         .padding(.top, 24)
       
       ScrollView {
+        ChartView(responses: responses)
         ForEach(responses) { response in
           ResponseView(response: response)
         }
